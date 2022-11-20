b51806122b1390b0a6845ecd3b425d6f441b88a9
TengMCing <llreczx@gmail.com>
2022-11-21 00:46:30 +1100

    remove untested code from main

--- main.c
+++ main.c
@@ -1,77 +1,25 @@
-#include "vdlmem.h"
-// #include "vdlportal.h"
-// #include "vdlseq.h"
+#include "vdlbt.h"
+#include <stdio.h>
 
-int main()
+#define foo() vdl_bt_call(foo_BT, int)
+
+static int foo_BT(vdl_bt bt)
 {
-    // vp v = vec_New(VEC_VP, 3);
-    // vp v0 = vec_New(VEC_CHAR, 3);
-    // vp v1 = vec_New(VEC_INT, 3);
-    // vp v2 = vec_New(VEC_VP, 4);
-    // vp v20 = vec_New(VEC_CHAR, 3);
-    // vp v21 = vec_New(VEC_CHAR, 3);
-    // vp v22 = vec_New(VEC_CHAR, 3);
-    //
-    // vec_Append(v, &v0);
-    // vec_Append(v, &v1);
-    // vec_Append(v, &v2);
-    //
-    // vec_Append(v2, &v20);
-    // vec_Append(v2, &v21);
-    // vec_Append(v2, &v22);
-    //
-    // vec_Pop(v2, 0);
-    // // printf("%p\n", vdl_vp_Array(v2->data)[1]);
-    //
-    // vec_Extend(v0, 3, &(char[]){65, 66, 67});
-    // vec_Concatenate(v2, v);
-    // // vec_Append(v2, &v);
-    //
-    // vec_GCDirectlyReachable(v);
-    // vec_GCDirectlyReachable(v2);
-    // vec_GCDirectlyUnreachable(v);
-    // vec_GCPrintArena(VDL_GDREACHABLE);
-    // vec_GCPrintArena(VDL_GREACHABLE);
-    // vec_GCPrintArena(VDL_GARENA);
-    // vec_GCCleanUp();
-    // vec_GCPrintArena(VDL_GDREACHABLE);
-    // vec_GCPrintArena(VDL_GREACHABLE);
-    // vec_GCPrintArena(VDL_GARENA);
-    // vec_GCKill();
-
-    vdl_V(vdl_V(1, 2, 3));
-
-    vdl_vp v = vdl_V(vdl_V(1, 2, 3), vdl_V(1.1, 2.2, 3.3), vdl_V(*"a", *"b", *"c"));
-
-    // vdl_vp vv = vdl_vp_ArgV(vdl_ArgV(1, 2, 3, 4, 5),
-    //                         vdl_ArgV(1.1, 2.2, 3.3),
-    //                         vdl_ArgV(*"a", *"b", *"c"));
-
-    void *try_val;
-    vdl_Try(VDL_SUPPRESS_OFF)
-    {
-        try_val = vdl_GetVp(v, 4);
-        vdl_ExceptFor(2)
-        {
-            printf("Index out of bound error handled!\n");
-        }
-    }
-
-    printf("%s\n", vdl_TypeStr(vdl_ArgV(*"a", *"b", *"c")));
-
-    printf("%s %zu\n", vdl_ModeStr(v), vdl_SizeOfData(vdl_GetVp(v, 0)));
-
-    printf("%f\n", vdl_GetDouble(vdl_ArgV(1.1, 2.2, 3.3), 0));
-
-    printf("%d\n", vdl_GetInt(vdl_GetVp(v, 0), 2));
-
-    // vdl_Indexing(v, vdl_ArgV(0));
-
-    // printf("%s\n", vdl_TypeStr(vdl_Indexing(v, vdl_IntSeq(0, 10))));
-    vdl_gc_Kill();
-    return 0;
-
+    vdl_bt_push(bt);
+    foo();
+    return 1;
 VDL_EXCEPTION:
-    printf("here\n");
-    return 0;
+    vdl_err_finally() printf("done!");
+    return 1;
 }
+
+
+int main(void)
+{
+    foo();
+    return 0;
+VDL_EXCEPTION:
+    vdl_err_abort();
+}
+
+// TODO:ADD module (e.g. util) to name
