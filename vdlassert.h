1d4993b7adb6b43bfe329ed7b074c9c1acd61094
TengMCing <llreczx@gmail.com>
2022-11-22 03:12:23 +1100

    rework vdlassert.h

--- vdlassert.h
+++ vdlassert.h
@@ -7,69 +7,93 @@
 
 #include "vdlbt.h"
 #include "vdldef.h"
-#include <stdarg.h>
 
 /*-----------------------------------------------------------------------------
  |  Expect
  ----------------------------------------------------------------------------*/
 
-static inline void vdl_internal_PrintError(const VDL_ERROR_CODE error_code, const char *spec, ...)
-{
-    VDL_INTERNAL_GERROR = error_code;
-    vdl_bt_Print();
-    va_list ap;
-    va_start(ap, spec);
-    vprintf(spec, ap);
-    va_end(ap);
-}
-
-#define vdl_PrintError()
-
 /// @description Except an expression to be true.
 /// @param expr (int). An boolean expression.
-/// @param error_code (VDL_ERROR_CODE). The error code.
-/// @param errmsg Some statements, usually a printf call.
-#define vdl_internal_Expect(expr, error_code, errmsg, ...)                              \
-    do {                                                                                \
-        if (!(expr))                                                                    \
-        {                                                                               \
-            vdl_internal_PrintError(error_code,                                         \
-                                    "[%d] Error raised by <%s> at %s:%d: " errmsg "\n", \
-                                    error_code,                                         \
-                                    __func__,                                           \
-                                    __FILE__,                                           \
-                                    __LINE__,                                           \
-                                    ##__VA_ARGS__);                                     \
-            VDL_THROW;                                                                  \
-        }                                                                               \
+/// @param code (VDL_ERR). The error code.
+/// @param errmsg specifier.
+/// @param ... Arguments passed to printf.
+#define vdlint_expect(expr, code, errmsg, ...)                                \
+    do {                                                                      \
+        if (!(expr))                                                          \
+        {                                                                     \
+            VDLINT_GERR.CODE = code;                                          \
+            VDLINT_GERR.MSG  = errmsg;                                        \
+            if (VDL_ERR_MSG_ON)                                               \
+            {                                                                 \
+                vdl_bt_print();                                               \
+                printf("[E%03d] Error raised by <%s> at %s:%d: " errmsg "\n", \
+                       code,                                                  \
+                       __func__,                                              \
+                       __FILE__,                                              \
+                       __LINE__,                                              \
+                       ##__VA_ARGS__);                                        \
+            }                                                                 \
+            goto VDL_EXCEPTION;                                               \
+        }                                                                     \
     } while (0)
+
 
 /*-----------------------------------------------------------------------------
  |  Assert
  ----------------------------------------------------------------------------*/
 
-#define vdl_assert_NullPointer(object) vdl_internal_Expect((object) != NULL, VDL_ERROR_NULL_POINTER, "NULL pointer provided!")
-#define vdl_assert_IndexOutOfBound(v, i) vdl_internal_Expect((i) >= 0 && (i) < (v)->length, VDL_ERROR_INDEX_OUT_OF_BOUND, "Index out of bound! Index [%d] not in [0, %d]!", i, (v)->length)
-#define vdl_assert_UnknownType(type) vdl_internal_Expect((type) >= VDL_TYPE_CHAR && (type) <= VDL_TYPE_VP, VDL_ERROR_UNKNOWN_TYPE, "Unknown type [%d] provided!", type)
-#define vdl_assert_NotStackAllocated(mode) vdl_internal_Expect((mode) == VDL_MODE_STACK, VDL_ERROR_NOT_STACK_ALLOCATED, "Storage mode [%d] is not stack allocation!", mode)
-#define vdl_assert_NotHeapAllocated(mode) vdl_internal_Expect((mode) == VDL_MODE_HEAP, VDL_ERROR_NOT_HEAP_ALLOCATED, "Storage mode [%d] is not heap allocation!", mode)
-#define vdl_assert_IncompatibleType(type1, type2) vdl_internal_Expect((type1) == (type2), VDL_ERROR_INCOMPATIBLE_TYPE, "Incompatible types (%d != %d) provided!", type1, type2)
-#define vdl_assert_ZeroCapacity(capacity) vdl_internal_Expect((capacity) > 0, VDL_ERROR_ZERO_CAPACITY, "Zero or negative capacity [%d] provided!", capacity)
-#define vdl_assert_ZeroLengthVector(v) vdl_internal_Expect((v)->length > 0, VDL_ERROR_ZERO_LENGTH_VECTOR, "Vector of zero length [%d] provided!", (v)->length)
-#define vdl_assert_ZeroObjects(num_object) vdl_internal_Expect((num_object) > 0, VDL_ERROR_ZERO_OBJECTS, "Zero or negative objects [%d] provided!", num_object)
+/// @description Assert null pointer.
+/// @param p (void *). A pointer.
+#define vdl_assert_null_pointer(p) vdlint_expect((p) != NULL, VDL_ERR_NULL_POINTER, "NULL pointer provided!")
 
-#define vdl_assert_UninitializedGC() vdl_internal_Expect(VDL_GARENA != NULL && VDL_GREACHABLE != NULL && VDL_GDREACHABLE != NULL, 11, "Garbage collector is uninitialized!")
-#define vdl_assert_InconsistentGC() vdl_internal_Expect(((VDL_GARENA == NULL) + (VDL_GREACHABLE == NULL) + (VDL_GDREACHABLE == NULL)) % 3 == 0, 12, "Garbage collector in an inconsistent state")
-#define vdl_assert_Unimplemented() vdl_internal_Expect(0, 13, "This function is unimplemented!")
+/// @description Assert index out of bound.
+/// @param v (vdl_vp). A vector.
+/// @param i (int). An integer.
+#define vdl_assert_idx_out_of_bound(v, i) vdlint_expect((i) >= 0 && (i) < (v)->len, VDL_ERR_INDEX_OUT_OF_BOUND, "Index out of bound! Index [%d] not in [0, %d]!", i, (v)->len)
+
+/// @description Assert unknown type.
+/// @param type (VDL_TYPE). A type.
+#define vdl_assert_unknown_type(type) vdlint_expect((type) >= VDL_TYPE_CHAR && (type) <= VDL_TYPE_VP, VDL_ERR_UNKNOWN_TYPE, "Unknown type [%d] provided!", type)
+
+/// @description Assert heap mode.
+/// @param mode (VDL_MODE). A mode.
+#define vdl_assert_heap_mode(mode) vdlint_expect((mode) == VDL_MODE_STACK, VDL_ERR_INCOMPATIBLE_MODE, "Storage mode [%d] is not stack allocation!", mode)
+
+/// @description Assert stack mode.
+/// @param mode (VDL_MODE). A mode.
+#define vdl_assert_stack_mode(mode) vdlint_expect((mode) == VDL_MODE_HEAP, VDL_ERR_INCOMPATIBLE_MODE, "Storage mode [%d] is not heap allocation!", mode)
+
+/// @description Assert incompatible type.
+/// @param type1 (VDL_TYPE). A type.
+/// @param type2 (VDL_TYPE). A type.
+#define vdl_assert_incompatible_type(type1, type2) vdlint_expect((type1) == (type2), VDL_ERR_INCOMPATIBLE_TYPE, "Incompatible types (%d != %d) provided!", type1, type2)
+
+/// @description Assert when try to create vector with 0 capacity.
+/// @param cap (int). Capacity.
+#define vdl_assert_zero_cap(cap) vdlint_expect((cap) > 0, VDL_ERR_ALLOCATE_FAIL, "Fail to allocate memory for zero or negative capacity [%d]!", cap)
+
+/// @description Assert when fail to allocate memory.
+/// @param p (void *). A pointer returned by malloc, calloc or realloc.
+#define vdl_assert_alloc_failed(p) vdlint_expect((p) != NULL, VDL_ERR_ALLOCATE_FAIL, "Fail to allocate memory!")
+
+#define vdl_assert_UninitializedGC() vdlint_expect(VDL_INTERNAL_GARENA != NULL && VDL_INTERNAL_GREACHABLE != NULL && VDL_INTERNAL_GDREACHABLE != NULL, VDL_ERROR_UNINITIALIZED_GC, "Garbage collector is uninitialized!")
+#define vdl_assert_InconsistentGC() vdlint_expect(((VDL_INTERNAL_GARENA == NULL) + (VDL_INTERNAL_GREACHABLE == NULL) + (VDL_INTERNAL_GDREACHABLE == NULL)) % 3 == 0, VDL_ERROR_INCONSISTENT_GC, "Garbage collector in an inconsistent state")
+#define vdl_assert_Unimplemented() vdlint_expect(0, VDL_ERROR_UNIMPLEMENTED, "This function is unimplemented!")
 
 /*-----------------------------------------------------------------------------
  |  Vector health check
  ----------------------------------------------------------------------------*/
 
-#define vdl_HealthCheck(v)                 \
-    do {                                   \
-        vdl_assert_NullPointer(v);         \
-        vdl_assert_UnknownType((v)->type); \
+/// @description Health check ensures the vector is not a null pointer (safe to read the metadata of the vector),
+/// the type is known (safe to do type dispatch) and the data pointer is not a null pointer (safe to use the data).
+#define vdl_assert_vec_health(v, nullv, utype, nulldat) \
+    do {                                                \
+        if (nullv)                                      \
+            vdl_assert_null_pointer(v);                 \
+        if (utype)                                      \
+            vdl_assert_unknown_type((v)->type);         \
+        if (nulldat)                                    \
+            vdl_assert_null_pointer((v)->dat);          \
     } while (0)
 
 #endif//VDL_VDLASSERT_H
