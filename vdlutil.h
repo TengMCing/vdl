197d4c628d012700c8d83dd4ea10fb83ef16abcc
TengMCing <llreczx@gmail.com>
2022-11-22 03:11:18 +1100

    make overloading for loop

--- vdlutil.h
+++ vdlutil.h
@@ -2,6 +2,7 @@
 // Created by Patrick Li on 25/9/22.
 //
 
+
 #ifndef VDL_VDLUTIL_H
 #define VDL_VDLUTIL_H
 
@@ -35,10 +36,18 @@
 /// @param ... Arguments.
 #define vdl_util_fnarg(n, ...) vdlint_util_f##n##arg(__VA_ARGS__)
 
+/*-----------------------------------------------------------------------------
+ |  Count the number of arguments
+ ----------------------------------------------------------------------------*/
+
 /// @description Count the number of arguments.
 /// @param ... Arguments.
 #define vdl_util_narg(...) vdlint_util_narg(0, ##__VA_ARGS__, 70, 69, 68, 67, 66, 65, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
 #define vdlint_util_narg(_0, _1_, _2_, _3_, _4_, _5_, _6_, _7_, _8_, _9_, _10_, _11_, _12_, _13_, _14_, _15_, _16_, _17_, _18_, _19_, _20_, _21_, _22_, _23_, _24_, _25_, _26_, _27_, _28_, _29_, _30_, _31_, _32_, _33_, _34_, _35_, _36, _37, _38, _39, _40, _41, _42, _43, _44, _45, _46, _47, _48, _49, _50, _51, _52, _53, _54, _55, _56, _57, _58, _59, _60, _61, _62, _63, _64, _65, _66, _67, _68, _69, _70, count, ...) count
+
+/*-----------------------------------------------------------------------------
+ |  Remove first n arguments
+ ----------------------------------------------------------------------------*/
 
 #define vdlint_util_rmf0arg(...) __VA_ARGS__
 #define vdlint_util_rmf1arg(first_arg, ...) __VA_ARGS__
@@ -68,15 +77,41 @@
 #define vdl_util_rmfnarg(n, ...) vdlint_util_rmf##n##arg(__VA_ARGS__)
 
 /*-----------------------------------------------------------------------------
- |  Loop through the length
+ |  Get the nth argument
  ----------------------------------------------------------------------------*/
 
-/// @description For i from 0 to ub - 1.
-/// @param ub (int). The upper bound.
-#define vdl_util_for_i(ub) for (int i = 0; i < (ub); i++)
-/// @description For j from 0 to ub - 1.
-/// @param ub (int). The upper bound.
-#define vdl_util_for_j(ub) for (int j = 0; j < (ub); j++)
+/// @description Get the first argument.
+#define vdl_util_getarg1(arg1, ...) arg1
+/// @description Get the second argument.
+#define vdl_util_getarg2(arg1, arg2, ...) arg2
+/// @description Get the third argument.
+#define vdl_util_getarg3(arg1, arg2, arg3, ...) arg3
+/// @description Get the fourth argument.
+#define vdl_util_getarg4(arg1, arg2, arg3, arg4, ...) arg4
+/// @description Get the fifth argument.
+#define vdl_util_getarg5(arg1, arg2, arg3, arg4, arg5, ...) arg5
+
+/*-----------------------------------------------------------------------------
+ |  For loop
+ ----------------------------------------------------------------------------*/
+
+/// @description A variadic macro similar to `range()` in python. The counter i is an int.
+/// @details One argument is end. Two arguments are start and end. Three arguments are start, end and step.
+#define vdl_for_i(...) vdl_util_getarg4(__VA_ARGS__, vdlint_util_for_i_3, vdlint_util_for_i_2, vdlint_util_for_i_1)(__VA_ARGS__)
+
+/// @description A variadic macro similar to `range()` in python. The counter j is an int.
+/// @details One argument is end. Two arguments are start and end. Three arguments are start, end and step.
+#define vdl_for_j(...) vdl_util_getarg4(__VA_ARGS__, vdlint_util_for_j_3, vdlint_util_for_j_2, vdlint_util_for_j_1)(__VA_ARGS__)
+
+#define vdlint_util_for_i_1(end) for (int i = 0; i < (end); i++)
+#define vdlint_util_for_j_1(end) for (int j = 0; j < (end); j++)
+
+#define vdlint_util_for_i_2(start, end) for (int i = start; i < (end); i++)
+#define vdlint_util_for_j_2(start, end) for (int j = start; j < (end); j++)
+
+#define vdlint_util_for_i_3(start, end, step) for (int i = start; i < (end); i += (step))
+#define vdlint_util_for_j_3(start, end, step) for (int j = start; j < (end); j += (step))
+
 
 /*-----------------------------------------------------------------------------
  |  Unused arguments
