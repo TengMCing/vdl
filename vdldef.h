67153a369f93afe5e86a150f7f6b74a75073faba
TengMCing <llreczx@gmail.com>
2022-12-09 23:49:00 +1100

    clean import

--- vdldef.h
+++ vdldef.h
@@ -7,7 +7,6 @@
 #ifndef VDL_VDLDEF_H
 #define VDL_VDLDEF_H
 
-#include "vdlutil.h"
 #include <limits.h>
 #include <math.h>
 #include <stdlib.h>
@@ -30,10 +29,10 @@ typedef enum VDL_TYPE
     VDL_TYPE_VP     = 3
 } VDL_TYPE;
 
-// #define vdl_lower_type_char VDL_TYPE_CHAR
-// #define vdl_lower_type_int VDL_TYPE_INT
-// #define vdl_lower_type_double VDL_TYPE_DOUBLE
-// #define vdl_lower_type_vdl_vp VDL_TYPE_VP
+#define vdl_lower_type_char VDL_TYPE_CHAR
+#define vdl_lower_type_int VDL_TYPE_INT
+#define vdl_lower_type_double VDL_TYPE_DOUBLE
+#define vdl_lower_type_vdl_vp VDL_TYPE_VP
 
 /// @description String representation of primitive vector types.
 static const char *const VDL_TYPE_STR[4] = {
