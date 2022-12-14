c4e9fb24c78d95ca258a3001fd840c3a24b0526f
TengMCing <llreczx@gmail.com>
2022-12-15 02:06:05 +1100

    rework vdlint_CheckVecHealth api

--- vdlassert.h
+++ vdlassert.h
@@ -17,7 +17,7 @@
 /// @param code (VDL_ERR). The error code.
 /// @param errmsg specifier.
 /// @param ... Arguments passed to printf.
-#define vdlint_assert_Expect(expr, code, errmsg, ...)                         \
+#define vdlint_e_Expect(expr, code, errmsg, ...)                              \
     do {                                                                      \
         if (!(expr))                                                          \
         {                                                                     \
@@ -26,6 +26,7 @@
             if (VDL_ERR_MSG_ON)                                               \
             {                                                                 \
                 vdlint_PrintBacktrace();                                      \
+                printf("\n");                                                 \
                 printf("[E%03d] Error raised by <%s> at %s:%d: " errmsg "\n", \
                        code,                                                  \
                        __func__,                                              \
@@ -44,53 +45,60 @@
 
 /// @description Assert null pointer.
 /// @param p (void *). A pointer.
-#define vdlint_CheckNullPointer(p) vdlint_assert_Expect((p) != NULL, VDL_ERR_NULL_POINTER, "NULL pointer provided!")
+#define vdlint_e_CheckNullPointer(p) vdlint_e_Expect((p) != NULL, VDL_ERR_NULL_POINTER, "NULL pointer provided!")
 
 /// @description Assert index out of bound.
 /// @param v (vdl_vp). A vector.
 /// @param i (int). An integer.
-#define vdlint_CheckIndexOutOfBound(v, i) vdlint_assert_Expect((i) >= 0 && (i) < (v)->len, VDL_ERR_INDEX_OUT_OF_BOUND, "Index out of bound! Index [%d] not in [0, %d]!", i, (v)->len)
+#define vdlint_e_CheckIndexOutOfBound(v, i) vdlint_e_Expect((i) >= 0 && (i) < (v)->len, VDL_ERR_INDEX_OUT_OF_BOUND, "Index out of bound! Index [%d] not in [0, %d)!", i, (v)->len)
 
 /// @description Assert unknown type.
 /// @param type (VDL_TYPE). A type.
-#define vdlint_CheckUnknownType(type) vdlint_assert_Expect((type) >= VDL_TYPE_CHAR && (type) <= VDL_TYPE_VP, VDL_ERR_UNKNOWN_TYPE, "Unknown type [%d] provided!", type)
+#define vdlint_e_CheckUnknownType(type) vdlint_e_Expect((type) >= VDL_TYPE_CHAR && (type) <= VDL_TYPE_VP, VDL_ERR_UNKNOWN_TYPE, "Unknown type [%d] provided!", type)
 
 /// @description Assert incompatible mode.
 /// @param mode1 (VDL_MODE). A mode.
 /// @param mode2 (VDL_MODE). Another mode.
-#define vdlint_CheckIncompatibleMode(mode1, mode2) vdlint_assert_Expect((mode1) == (mode2), VDL_ERR_INCOMPATIBLE_MODE, "Incompatible storage modes (%d != %d) provided!", mode1, mode2)
+#define vdlint_e_CheckIncompatibleMode(mode1, mode2) vdlint_e_Expect((mode1) == (mode2), VDL_ERR_INCOMPATIBLE_MODE, "Incompatible storage modes (%d != %d) provided!", mode1, mode2)
 
 /// @description Assert incompatible type.
 /// @param type1 (VDL_TYPE). A type.
 /// @param type2 (VDL_TYPE). A type.
-#define vdlint_CheckIncompatibleType(type1, type2) vdlint_assert_Expect((type1) == (type2), VDL_ERR_INCOMPATIBLE_TYPE, "Incompatible types (%d != %d) provided!", type1, type2)
+#define vdlint_e_CheckIncompatibleType(type1, type2) vdlint_e_Expect((type1) == (type2), VDL_ERR_INCOMPATIBLE_TYPE, "Incompatible types (%d != %d) provided!", type1, type2)
 
 /// @description Assert when try to create vector with 0 cap.
 /// @param cap (int). Capacity.
-#define vdlint_CheckAllocZeroCap(cap) vdlint_assert_Expect((cap) > 0, VDL_ERR_ALLOCATE_FAILED, "Fail to allocate memory for zero or negative cap [%d]!", cap)
+#define vdlint_e_CheckAllocZeroCap(cap) vdlint_e_Expect((cap) > 0, VDL_ERR_ALLOCATE_FAILED, "Fail to allocate memory for zero or negative cap [%d]!", cap)
 
 /// @description Assert when fail to allocate memory.
 /// @param p (void *). A pointer returned by malloc, calloc or realloc.
-#define vdlint_CheckAllocFailed(p) vdlint_assert_Expect((p) != NULL, VDL_ERR_ALLOCATE_FAILED, "Fail to allocate memory!")
+#define vdlint_e_CheckAllocFailed(p) vdlint_e_Expect((p) != NULL, VDL_ERR_ALLOCATE_FAILED, "Fail to allocate memory!")
 
-#define vdlint_CheckUninitializedGC() vdlint_assert_Expect(VDLINT_GARENA != NULL && VDLINT_GREACHABLE != NULL && VDLINT_GDREACHABLE != NULL, VDL_ERR_GC_FAIL, "Garbage collector is uninitialized!")
-#define vdlint_CheckInconsistentGC() vdlint_assert_Expect(((VDLINT_GARENA == NULL) + (VDLINT_GREACHABLE == NULL) + (VDLINT_GDREACHABLE == NULL)) % 3 == 0, VDL_ERR_GC_FAIL, "Garbage collector in an inconsistent state")
-#define vdlint_CheckUnimplemented() vdlint_assert_Expect(0, VDL_ERR_UNIMPLEMENTED, "This function is unimplemented!")
+#define vdlint_e_CheckUninitializedGC() vdlint_e_Expect(VDLINT_GARENA != NULL && VDLINT_GREACHABLE != NULL && VDLINT_GDREACHABLE != NULL, VDL_ERR_GC_FAIL, "Garbage collector is uninitialized!")
+#define vdlint_e_CheckInconsistentGC() vdlint_e_Expect(((VDLINT_GARENA == NULL) + (VDLINT_GREACHABLE == NULL) + (VDLINT_GDREACHABLE == NULL)) % 3 == 0, VDL_ERR_GC_FAIL, "Garbage collector in an inconsistent state")
+#define vdlint_e_CheckUnimplemented() vdlint_e_Expect(0, VDL_ERR_UNIMPLEMENTED, "This function is unimplemented!")
 
 /*-----------------------------------------------------------------------------
  |  Vector health check
  ----------------------------------------------------------------------------*/
 
+#define VDLINT_CHECK_NULL_VEC 0x1
+#define VDLINT_CHECK_UNKNOWN_VEC_TYPE 0x2
+#define VDLINT_CHECK_NULL_VEC_DAT 0x4
+
 /// @description Health check ensures the vector is not a null pointer (safe to read the metadata of the vector),
 /// the type is known (safe to do type dispatch) and the data pointer is not a null pointer (safe to use the data).
-#define vdlint_CheckVecHealth(v, nullv, utype, nulldat) \
-    do {                                                \
-        if (nullv)                                      \
-            vdlint_CheckNullPointer(v);                 \
-        if (utype)                                      \
-            vdlint_CheckUnknownType((v)->type);         \
-        if (nulldat)                                    \
-            vdlint_CheckNullPointer((v)->dat);          \
+/// @details Three macros are available to be combined by the or operator.
+/// These are VDLINT_CHECK_NULL_VEC, VDLINT_CHECK_UNKNOWN_VEC_TYPE and VDLINT_CHECK_NULL_VEC_DAT.
+#define vdlint_e_CheckVec(v, check)                  \
+    do {                                             \
+        if (VDLINT_CHECK_NULL_VEC & (check))         \
+            vdlint_e_CheckNullPointer(v);            \
+        if (VDLINT_CHECK_UNKNOWN_VEC_TYPE & (check)) \
+            vdlint_e_CheckUnknownType((v)->type);    \
+        if (VDLINT_CHECK_NULL_VEC_DAT & (check))     \
+            vdlint_e_CheckNullPointer((v)->dat);     \
     } while (0)
+
 
 #endif//VDL_VDLASSERT_H
