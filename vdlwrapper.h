a2690e14b8b2c362a8b686e1a4c600648d492b89
TengMCing <llreczx@gmail.com>
2022-12-15 02:06:51 +1100

    template functions in vdlwrapper

--- vdlwrapper.h
+++ vdlwrapper.h
@@ -13,35 +13,35 @@
 
 /// @description Cast a void pointer to a char pointer
 /// @param voidp (void *). A void pointer.
-#define vdlint_CharArray(voidp) ((char *) (voidp))
+#define vdlint_AsCharArray(voidp) ((char *) (voidp))
 
 /// @description Cast a void pointer to an int pointer
 /// @param voidp (void *). A void pointer.
-#define vdlint_IntArray(voidp) ((int *) (voidp))
+#define vdlint_AsIntArray(voidp) ((int *) (voidp))
 
 /// @description Cast a void pointer to a double pointer
 /// @param voidp (void *). A void pointer.
-#define vdlint_DoubleArray(voidp) ((double *) (voidp))
+#define vdlint_AsDoubleArray(voidp) ((double *) (voidp))
 
 /// @description Cast a void pointer to a vdl_vp pointer
 /// @param voidp (void *). A void pointer.
-#define vdlint_VpArray(voidp) ((vdl_vp *) (voidp))
+#define vdlint_AsVpArray(voidp) ((vdl_vp *) (voidp))
 
 /// @description Cast a void pointer to a char pointer and get the first element.
 /// @param voidp (void *). A void pointer.
-#define vdlint_CharArray0(voidp) (vdlint_CharArray(voidp)[0])
+#define vdlint_CharArray0(voidp) (vdlint_AsCharArray(voidp)[0])
 
 /// @description Cast a void pointer to an int pointer and get the first element.
 /// @param voidp (void *). A void pointer.
-#define vdlint_IntArray0(voidp) (vdlint_IntArray(voidp)[0])
+#define vdlint_IntArray0(voidp) (vdlint_AsIntArray(voidp)[0])
 
 /// @description Cast a void pointer to a double pointer and get the first element.
 /// @param voidp (void *). A void pointer.
-#define vdlint_DoubleArray0(voidp) (vdlint_DoubleArray(voidp)[0])
+#define vdlint_DoubleArray0(voidp) (vdlint_AsDoubleArray(voidp)[0])
 
 /// @description Cast a void pointer to a vdl_vp pointer and get the first element.
 /// @param voidp (void *). A void pointer.
-#define vdlint_VpArray0(voidp) (vdlint_VpArray(voidp)[0])
+#define vdlint_VpArray0(voidp) (vdlint_AsVpArray(voidp)[0])
 
 /*-----------------------------------------------------------------------------
  |  Type of a vector
@@ -49,11 +49,13 @@
 
 /// @description Type of a vector.
 /// @param v (vdl_vp). A vector.
+/// @return (VDL_TYPE) A vector type.
 #define vdl_GetType(v) ((v)->type)
 
 /// @description String representation of the type of a vector.
 /// @param v (vdl_vp). A vector.
-#define vdl_GetTypeStr(v) (VDL_TYPE_STR[vdl_GetType(v)])
+/// @return (const char *) A string.
+#define vdl_GetTypeStr(v) (VDL_TYPE_STR[(v)->type])
 
 /*-----------------------------------------------------------------------------
  |  Storage mode of a vector
@@ -61,11 +63,13 @@
 
 /// @description Mode of a vector.
 /// @param v (vdl_vp). A vector.
+/// @return (VDL_MODE) A vector mode.
 #define vdl_GetMode(v) ((v)->mode)
 
 /// @description String representation of the mode of a vector.
 /// @param v (vdl_vp). A vector.
-#define vdl_GetModeStr(v) (VDL_MODE_STR[vdl_GetMode(v)])
+/// @return (const char *) A string.
+#define vdl_GetModeStr(v) (VDL_MODE_STR[(v)->mode])
 
 /*-----------------------------------------------------------------------------
  |  Capacity of a vector
@@ -73,13 +77,8 @@
 
 /// @description Capacity of a vector.
 /// @param v (vdl_vp). A vector.
-/// @param (int) The cap.
-#define vdlint_Cap(v) ((v)->cap)
-
-/// @description Capacity of a vector.
-/// @param v (vdl_vp). A vector.
-/// @param (int) A copy of the cap.
-#define vdl_GetCap(v) ((int){vdlint_Cap(v)})
+/// @param (int) The capacity.
+#define vdl_GetCap(v) ((int){(v)->cap})
 
 /*-----------------------------------------------------------------------------
  |  Length of a vector
@@ -87,20 +86,8 @@
 
 /// @description Length of a vector.
 /// @param v (vdl_vp). A vector.
-#define vdlint_Len(v) ((v)->len)
-
-/// @description Length of a vector.
-/// @param v (vdl_vp). A vector.
-/// @param (int) A copy of the len.
-#define vdl_GetLen(v) ((int){vdlint_Len(v)})
-
-/*-----------------------------------------------------------------------------
- |  Data of the vector
- ----------------------------------------------------------------------------*/
-
-/// @description Data pointer of a vector.
-/// @param v (vdl_vp). A vector.
-#define vdlint_Dat(v) ((v)->dat)
+/// @param (int) The length.
+#define vdl_GetLen(v) ((int){(v)->len})
 
 /*-----------------------------------------------------------------------------
  |  Size of type or vector
@@ -117,40 +104,45 @@ static const size_t VDL_TYPE_SIZE[4] = {
 
 /// @description Get the size of the data of a vector.
 /// @param v (vdl_vec*). A vector.
-#define vdl_SizeOfDat(v) ((size_t) vdlint_Cap(v) * vdl_SizeOfType(vdl_GetType(v)))
+#define vdl_SizeOfDat(v) ((size_t) (v)->cap * vdl_SizeOfType((v)->type))
 
 /// @description Get the size of a vector.
 /// @param v (vdl_vec*). A vector.
-#define vdl_SizeOfVec(v) ((size_t) vdlint_Cap(v) * vdl_SizeOfType(vdl_GetType(v)) + sizeof(vdl_vec))
+#define vdl_SizeOfVec(v) ((size_t) (v)->cap * vdl_SizeOfType((v)->type) + sizeof(vdl_vec))
 
 /*-----------------------------------------------------------------------------
  |  Accessing the vector data
  ----------------------------------------------------------------------------*/
 
-/// @description Get the address of the ith item from the data of a vector. No checks will be performed.
+/// @description Get the pointer to the address of the ith item of a vector. No checks will be performed.
 /// @param v (vdl_vec*). A vector.
 /// @param i (int). An index.
-#define vdlint_Address(v, i) (vdlint_CharArray(vdlint_Dat(v)) + vdl_SizeOfType(vdl_GetType(v)) * (size_t) (i))
+/// @return (char *) A pointer to the address.
+#define vdlint_AddressOf(v, i) (vdlint_AsCharArray((v)->dat) + vdl_SizeOfType((v)->type) * (size_t) (i))
 
-/// @description Get the ith item from the data of a vector as char. No checks will be performed.
+/// @description Get the ith item a vector as char. No checks will be performed.
 /// @param v (vdl_vec*). A vector.
 /// @param i (int). An index.
-#define vdlint_CharAt(v, i) (vdlint_CharArray(vdlint_Dat(v))[i])
+/// @return (char) The item.
+#define vdlint_CharAt(v, i) (vdlint_AsCharArray((v)->dat)[i])
 
-/// @description Get the ith item from the data of a vector as int. No checks will be performed.
+/// @description Get the ith item of a vector as int. No checks will be performed.
 /// @param v (vdl_vec*). A vector.
 /// @param i (int). An index.
-#define vdlint_IntAt(v, i) (vdlint_IntArray(vdlint_Dat(v))[i])
+/// @return (int) The item.
+#define vdlint_IntAt(v, i) (vdlint_AsIntArray((v)->dat)[i])
 
-/// @description Get the ith item from the data of a vector as double. No checks will be performed.
+/// @description Get the ith item of a vector as double. No checks will be performed.
 /// @param v (vdl_vec*). A vector.
 /// @param i (int). An index.
-#define vdlint_DoubleAt(v, i) (vdlint_DoubleArray(vdlint_Dat(v))[i])
+/// @return (double) The item.
+#define vdlint_DoubleAt(v, i) (vdlint_AsDoubleArray((v)->dat)[i])
 
-/// @description Get the ith item from the data of a vector as vector pointer. No checks will be performed.
+/// @description Get the ith item of a vector as vdl_vp. No checks will be performed.
 /// @param v (vdl_vec*). A vector.
 /// @param i (int). An index.
-#define vdlint_VpAt(v, i) (vdlint_VpArray(vdlint_Dat(v))[i])
+/// @return (vdl_vp) The item.
+#define vdlint_VpAt(v, i) (vdlint_AsVpArray((v)->dat)[i])
 
 
 /// @description Get the address of an item from a vector. Boundary conditions will be checked.
@@ -158,13 +150,14 @@ static const size_t VDL_TYPE_SIZE[4] = {
 /// @param v (const vdl_vec *const). A vector.
 /// @param i (const int). Index of the item.
 /// @return (void*) Pointer to the item.
-#define vdl_Address(...) vdlint_Call(vdl_Address_BT, void *, __VA_ARGS__)
-static inline void *vdl_Address_BT(vdl_bt bt, const vdl_vec *const v, const int i)
+#define vdl_e_AddressOf(...) vdlint_e_Call(vdl_e_AddressOf_BT, void *, __VA_ARGS__)
+static inline void *vdl_e_AddressOf_BT(vdl_fr fr, const vdl_vec *const v, const int i)
 {
-    vdl_PushBacktrace(bt);
-    vdlint_CheckVecHealth(v, 1, 1, 1);
-    vdlint_CheckIndexOutOfBound(v, i);
-    return vdlint_Address(v, i);
+    vdl_e_PushFrame(fr);
+    vdlint_e_CheckVec(v, VDLINT_CHECK_NULL_VEC | VDLINT_CHECK_UNKNOWN_VEC_TYPE | VDLINT_CHECK_NULL_VEC_DAT);
+    vdlint_e_CheckIndexOutOfBound(v, i);
+
+    return vdlint_AddressOf(v, i);
 VDL_EXCEPTION:
     return NULL;
 }
@@ -174,68 +167,49 @@ VDL_EXCEPTION:
 /// @param v (const vdl_vec *const). A vector.
 /// @param i (const int). Index of the item.
 /// @return (char) A character.
-#define vdl_GetChar(...) vdlint_Call(vdl_GetChar_BT, char, __VA_ARGS__)
-static inline char vdl_GetChar_BT(vdl_bt bt, const vdl_vec *const v, const int i)
-{
-    vdl_PushBacktrace(bt);
-    vdlint_CheckVecHealth(v, 1, 0, 1);
-    vdlint_CheckIndexOutOfBound(v, i);
-    vdlint_CheckIncompatibleType(VDL_TYPE_CHAR, vdl_GetType(v));
-    return vdlint_CharAt(v, i);
-VDL_EXCEPTION:
-    return VDL_TYPE_CHAR_NA;
-}
+#define vdl_e_GetChar(...) vdlint_e_Call(vdl_e_GetChar_BT, char, __VA_ARGS__)
 
 /// @description Get the ith item of a vector and interpret it as an int.
 /// @details If any check fails, a missing value will be returned.
 /// @param v (const vdl_vec *const). A vector.
 /// @param i (const int). Index of the item.
 /// @return (int) An integer.
-#define vdl_GetInt(...) vdlint_Call(vdl_GetInt_BT, int, __VA_ARGS__)
-static inline int vdl_GetInt_BT(vdl_bt bt, const vdl_vec *const v, const int i)
-{
-    vdl_PushBacktrace(bt);
-    vdlint_CheckVecHealth(v, 1, 0, 1);
-    vdlint_CheckIndexOutOfBound(v, i);
-    vdlint_CheckIncompatibleType(VDL_TYPE_INT, vdl_GetType(v));
-    return vdlint_IntAt(v, i);
-VDL_EXCEPTION:
-    return VDL_TYPE_INT_NA;
-}
+#define vdl_e_GetInt(...) vdlint_e_Call(vdl_e_GetInt_BT, int, __VA_ARGS__)
 
 /// @description Get the ith item of a vector and interpret it as a double.
 /// @details If any check fails, a missing value will be returned.
 /// @param v (const vdl_vec *const). A vector.
 /// @param i (const int). Index of the item.
 /// @return (double) A double.
-#define vdl_GetDouble(...) vdlint_Call(vdl_GetDouble_BT, double, __VA_ARGS__)
-static inline double vdl_GetDouble_BT(vdl_bt bt, const vdl_vec *const v, const int i)
-{
-    vdl_PushBacktrace(bt);
-    vdlint_CheckVecHealth(v, 1, 0, 1);
-    vdlint_CheckIndexOutOfBound(v, i);
-    vdlint_CheckIncompatibleType(VDL_TYPE_DOUBLE, vdl_GetType(v));
-    return vdlint_DoubleAt(v, i);
-VDL_EXCEPTION:
-    return VDL_TYPE_DOUBLE_NA;
-}
+#define vdl_e_GetDouble(...) vdlint_e_Call(vdl_e_GetDouble_BT, double, __VA_ARGS__)
 
 /// @description Get the ith item of a vector and interpret it as a vector consists of vectors of any type.
 /// @details If any check fails, a missing value will be returned.
 /// @param v (const vdl_vec *const). A vector.
 /// @param i (const int). Index of the item.
 /// @return (vdl_vp) A vector.
-#define vdl_GetVp(...) vdlint_Call(vdl_GetVp_BT, vdl_vp, __VA_ARGS__)
-static inline vdl_vp vdl_GetVp_BT(vdl_bt bt, const vdl_vec *const v, const int i)
-{
-    vdl_PushBacktrace(bt);
-    vdlint_CheckVecHealth(v, 1, 0, 1);
-    vdlint_CheckIndexOutOfBound(v, i);
-    vdlint_CheckIncompatibleType(VDL_TYPE_VP, vdl_GetType(v));
-    return vdlint_VpAt(v, i);
-VDL_EXCEPTION:
-    return VDL_TYPE_VP_NA;
-}
+#define vdl_e_GetVp(...) vdlint_e_Call(vdl_e_GetVp_BT, vdl_vp, __VA_ARGS__)
+
+// TEMPLATE BEGIN
+#define vdlint_e_GetT_BT(T, CT, UT)                                                    \
+    static inline T vdl_e_Get##CT##_BT(vdl_fr fr, const vdl_vec *const v, const int i) \
+    {                                                                                  \
+        vdl_e_PushFrame(fr);                                                           \
+        vdlint_e_CheckVec(v, VDLINT_CHECK_NULL_VEC | VDLINT_CHECK_NULL_VEC_DAT);       \
+        vdlint_e_CheckIndexOutOfBound(v, i);                                           \
+        vdlint_e_CheckIncompatibleType(VDL_TYPE_##UT, v->type);                        \
+                                                                                       \
+        return vdlint_##CT##At(v, i);                                                  \
+    VDL_EXCEPTION:                                                                     \
+        return VDL_TYPE_##UT##_NA;                                                     \
+    }
+
+vdlint_e_GetT_BT(char, Char, CHAR);
+vdlint_e_GetT_BT(int, Int, INT);
+vdlint_e_GetT_BT(double, Double, DOUBLE);
+vdlint_e_GetT_BT(vdl_vp, Vp, VP);
+#undef vdlint_e_GetT_BT
+// TEMPLATE END
 
 /*-----------------------------------------------------------------------------
  |  Setting the vector data
@@ -243,42 +217,43 @@ VDL_EXCEPTION:
 
 /// @description Set a series of items for a vector. Boundary conditions will be checked.
 /// @details The user has to ensure the object is an array of the same type as the vector.
-/// The function has no way to check for it. If any check fails, no content will be written.
+/// The function has no way to check for it. If the number of objects less than 1, the
+/// function will do nothing. If any check fails, no content will be written.
 /// @param v (vdl_vec *const). A vector.
 /// @param i (const int). Index of the item.
-/// @param object (void*). An array of objects.
+/// @param array (void*). An array of objects.
 /// @param num_object (int). Number of objects.
-#define vdl_SetByArray(...) vdlint_CallVoid(vdl_SetByArray_BT, __VA_ARGS__)
-static inline void vdl_SetByArray_BT(vdl_bt bt, vdl_vec *const v, const int i, void *const object, const int num_object)
+#define vdl_e_SetByArray(...) vdlint_e_CallVoid(vdl_e_SetByArray_BT, __VA_ARGS__)
+static inline void vdl_e_SetByArray_BT(vdl_fr fr, vdl_vec *const v, const int i, void *const array, const int num_object)
 {
-    vdl_PushBacktrace(bt);
+    vdl_e_PushFrame(fr);
     if (num_object <= 0)
         return;
-    vdlint_CheckVecHealth(v, 1, 1, 1);
-    vdlint_CheckNullPointer(object);
-    vdlint_CheckIndexOutOfBound(v, i);
-    vdlint_CheckIndexOutOfBound(v, i + num_object - 1);
+    vdlint_e_CheckVec(v, VDLINT_CHECK_NULL_VEC | VDLINT_CHECK_UNKNOWN_VEC_TYPE | VDLINT_CHECK_NULL_VEC_DAT);
+    vdlint_e_CheckNullPointer(array);
+    vdlint_e_CheckIndexOutOfBound(v, i);
+    vdlint_e_CheckIndexOutOfBound(v, i + num_object - 1);
 
     // Optimize for the single object case
     if (num_object == 1)
-        switch (vdl_GetType(v))
+        switch (v->type)
         {
             case VDL_TYPE_CHAR:
-                vdlint_CharAt(v, i) = vdlint_CharArray0(object);
+                vdlint_CharAt(v, i) = vdlint_CharArray0(array);
                 return;
             case VDL_TYPE_INT:
-                vdlint_IntAt(v, i) = vdlint_IntArray0(object);
+                vdlint_IntAt(v, i) = vdlint_IntArray0(array);
                 return;
             case VDL_TYPE_DOUBLE:
-                vdlint_DoubleAt(v, i) = vdlint_DoubleArray0(object);
+                vdlint_DoubleAt(v, i) = vdlint_DoubleArray0(array);
                 return;
             case VDL_TYPE_VP:
-                vdlint_VpAt(v, i) = vdlint_VpArray0(object);
+                vdlint_VpAt(v, i) = vdlint_VpArray0(array);
                 return;
         }
 
-    // Copy in the memory
-    memmove(vdlint_Address(v, i), object, (size_t) num_object * vdl_SizeOfType(vdl_GetType(v)));
+    // Copy the content from the array to the vector
+    memmove(vdlint_AddressOf(v, i), array, (size_t) num_object * vdl_SizeOfType(v->type));
 VDL_EXCEPTION:
     return;
 }
@@ -289,16 +264,17 @@ VDL_EXCEPTION:
 /// @param i (int). The first index to be set.
 /// @param v2 (vdl_vec *const). An array of objects.
 /// @param num_object (int). Number of objects.
-#define vdl_Set(...) vdlint_CallVoid(vdl_Set_BT, __VA_ARGS__)
-static inline void vdl_Set_BT(vdl_bt bt, vdl_vec *const v1, const int i, vdl_vec *const v2)
+#define vdl_e_Set(...) vdlint_e_CallVoid(vdl_e_Set_BT, __VA_ARGS__)
+static inline void vdl_e_Set_BT(vdl_fr fr, vdl_vec *const v1, const int i, vdl_vec *const v2)
 {
-    vdl_PushBacktrace(bt);
+    vdl_e_PushFrame(fr);
     // Only need to ensure we can use the metadata of v1 and v2,
     // and v1 and v2 are of the same type
-    vdlint_CheckNullPointer(v1);
-    vdlint_CheckNullPointer(v2);
-    vdlint_CheckIncompatibleType(vdl_GetType(v1), vdl_GetType(v2));
-    vdl_SetByArray(v1, i, vdlint_Dat(v2), vdlint_Len(v2));
+    vdlint_e_CheckNullPointer(v1);
+    vdlint_e_CheckNullPointer(v2);
+    vdlint_e_CheckIncompatibleType(v1->type, v2->type);
+    vdl_e_SetByArray(v1, i, v2->dat, v2->len);
+
 VDL_EXCEPTION:
     return;
 }
