c4416f7836e5bd5b71e9d89aa0e76fe6a7a001c8
TengMCing <llreczx@gmail.com>
2022-12-15 02:07:54 +1100

    update main

--- main.c
+++ main.c
@@ -1,27 +1,38 @@
-#include "vdlmem.h"
+#include "vdlportal.h"
 #include <stdio.h>
 
-#define foo() vdlint_Call(foo_BT, int)
-
-static int foo_BT(vdl_bt bt)
-{
-    vdl_PushBacktrace(bt);
-    foo();
-    return 1;
-VDL_EXCEPTION:
-    vdl_Finally() printf("done!");
-    return 1;
-}
-
+// #define foo() vdlint_e_Call(foo_BT, int)
+//
+// static int foo_BT(vdl_fr bt)
+// {
+//     vdl_e_PushFrame(bt);
+//     foo();
+//     return 1;
+// VDL_EXCEPTION:
+//     vdl_Finally() printf("done!");
+//     return 1;
+// }
 
 int main(void)
 {
-    // foo();
-    vdl_LocV(*"a", 2, 3);
+    vdl_vp v = vdl_e_V(vdl_e_V(1, 2, 3),
+                       vdl_e_V(1.1, 2.2, 3.3),
+                       vdl_e_V(*"a", *"b", *"c"),
+                       vdl_e_V(NULL, NULL, NULL));
 
-    vdl_V(1, 2, 3);
+    vdl_vp vv = vdl_e_V(vdl_e_Get(v, 3), vdl_e_Get(v, 3), vdl_e_Get(v, 3), vdl_e_Get(v, 3));
+
+    vdl_e_Set(vdl_e_Get(v, 3), 0, vv);
+
+    printf("%d\n", v->len);
+
+    vdl_ne_GCDirectlyReachable(vdl_e_Get(v, 3));
+
+    vdl_ne_GCCleanUp();
+    vdl_ne_PrintArena(VDLINT_GARENA);
 
     return 0;
+
 VDL_EXCEPTION:
     vdl_Abort();
 }
