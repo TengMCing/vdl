0c9f911b3064616688ad87195b125d6944692ace
TengMCing <llreczx@gmail.com>
2023-02-23 10:11:17 +1100

    Fix integer overflow issue

--- include/vdl_5_garbage_collector_def.h
+++ include/vdl_5_garbage_collector_def.h
@@ -38,15 +38,12 @@ static inline void *vdl_Calloc_BT(const size_t count, const size_t bytes, const 
 static inline VDL_VECTOR_TABLE_P vdl_NewVectorTable_BT(void)
 {
     VDL_VECTOR_TABLE_T *vector_table = vdl_Malloc(sizeof(VDL_VECTOR_TABLE_T), 1);
-    void *data_container             = vdl_Malloc(VDL_VECTOR_TABLE_INIT_CAPACITY * sizeof(VDL_VECTOR_P), 1);
+    vector_table->Length             = 0;
+    vector_table->Capacity           = 0;
+    vector_table->Data               = vdl_Malloc(VDL_VECTOR_TABLE_INIT_CAPACITY * sizeof(VDL_VECTOR_P), 1);
 
-    // Construct the vector table
-    vector_table->Length   = 0;
-    vector_table->Capacity = 0;
-    vector_table->Data     = data_container;
-
-    vdl_ExceptionDeregisterCleanUp(local_vector_table);
-    vdl_ExceptionDeregisterCleanUp(local_data_container);
+    vdl_ExceptionDeregisterCleanUp(vector_table);
+    vdl_ExceptionDeregisterCleanUp(vector_table->Data);
 
     return vector_table;
 }
@@ -57,27 +54,32 @@ static inline void vdl_ShrinkVectorTable_BT(VDL_VECTOR_TABLE_T *const vector_tab
     vdl_CheckNullPointer(vector_table->Data);
 
     // Decide the target size
-    const int size_times_five = vector_table->Length * 5;
-    const int size_plus_5MB   = vector_table->Length + 700000;
-    int smaller_cap           = size_times_five > size_plus_5MB ? size_plus_5MB : size_times_five;
+    size_t size_times_five = (size_t) vector_table->Length * 5;
+    if (size_times_five > VDL_VECTOR_MAX_CAPACITY)
+        size_times_five = VDL_VECTOR_MAX_CAPACITY;
+    size_t size_plus_5MB = (size_t) vector_table->Length + 700000;
+    if (size_plus_5MB > VDL_VECTOR_MAX_CAPACITY)
+        size_plus_5MB = VDL_VECTOR_MAX_CAPACITY;
+    size_t smaller_cap = size_times_five > size_plus_5MB ? size_plus_5MB : size_times_five;
     if (smaller_cap < VDL_VECTOR_TABLE_INIT_CAPACITY)
         smaller_cap = VDL_VECTOR_TABLE_INIT_CAPACITY;
-    if (vector_table->Capacity <= smaller_cap)
+
+    if ((size_t) vector_table->Capacity <= smaller_cap)
         return;
 
-    size_t target_size = (size_t) smaller_cap * sizeof(VDL_VECTOR_P);
+    size_t target_size = smaller_cap * sizeof(VDL_VECTOR_P);
 
     // Reallocate memory for the data container
     void *buffer = vdl_Malloc(target_size, 1);
     memcpy(buffer, vector_table->Data, target_size);
     vdl_Free(vector_table->Data);
     vector_table->Data     = buffer;
-    vector_table->Capacity = smaller_cap;
+    vector_table->Capacity = (int) smaller_cap;
 
     vdl_ExceptionDeregisterCleanUp(buffer);
 }
 
-static inline void vdl_CleanVectorTable_BT(VDL_VECTOR_TABLE_T *const vector_table, const int free_content)
+static inline void vdl_ClearVectorTable_BT(VDL_VECTOR_TABLE_P vector_table, int free_content)
 {
     vdl_CheckNullPointer(vector_table);
     vdl_CheckNullPointer(vector_table->Data);
@@ -99,6 +101,9 @@ static inline void vdl_CleanVectorTable_BT(VDL_VECTOR_TABLE_T *const vector_tabl
             vdl_Free(vectors[i]);
         }
     }
+
+    // Deallocate unused space
+    vdl_ShrinkVectorTable(vector_table);
 }
 
 static inline void vdl_DeleteVectorTable_BT(VDL_VECTOR_TABLE_T *const vector_table, const int free_content)
@@ -131,7 +136,6 @@ static inline void vdl_DeleteVectorTable_BT(VDL_VECTOR_TABLE_T *const vector_tab
 
 static inline void vdl_ReserveForVectorTable_BT(VDL_VECTOR_TABLE_T *const vector_table, const int capacity)
 {
-
     vdl_CheckNullPointer(vector_table);
     vdl_CheckNullPointer(vector_table->Data);
     vdl_Expect(capacity <= VDL_VECTOR_TABLE_MAX_CAPACITY,
@@ -145,24 +149,24 @@ static inline void vdl_ReserveForVectorTable_BT(VDL_VECTOR_TABLE_T *const vector
         return;
 
     // Decide the new capacity
-    int new_capacity = vector_table->Capacity;
-    while (new_capacity < capacity)
+    size_t new_capacity = (size_t) vector_table->Capacity;
+    while (new_capacity < (size_t) capacity)
     {
         static const size_t MEM_500KB = 500 * 1024;
-        if ((size_t) new_capacity * sizeof(VDL_VECTOR_P) < MEM_500KB)
+        if (new_capacity * sizeof(VDL_VECTOR_P) < MEM_500KB)
             new_capacity = new_capacity * 2 + 8;
         else
-            new_capacity += (int) (MEM_500KB / sizeof(VDL_VECTOR_P));
+            new_capacity += MEM_500KB / sizeof(VDL_VECTOR_P);
     }
     if (new_capacity > VDL_VECTOR_TABLE_MAX_CAPACITY)
         new_capacity = VDL_VECTOR_TABLE_MAX_CAPACITY;
 
-    size_t target_size = (size_t) new_capacity * sizeof(VDL_VECTOR_P);
+    size_t target_size = new_capacity * sizeof(VDL_VECTOR_P);
 
     // Attempt to reallocate memory
     void *buffer           = vdl_Malloc(target_size, 1);
     vector_table->Data     = buffer;
-    vector_table->Capacity = new_capacity;
+    vector_table->Capacity = (int) new_capacity;
 
     vdl_ExceptionDeregisterCleanUp(buffer);
 }
@@ -204,6 +208,9 @@ static inline int vdl_FindInVectorTable_BT(VDL_VECTOR_TABLE_T *const vector_tabl
 {
     vdl_CheckNullPointer(vector_table);
     vdl_CheckNullPointer(vector_table->Data);
+
+    if (v == NULL)
+        return -1;
 
     VDL_VECTOR_CONST_POINTER_ARRAY vectors = vector_table->Data;
     vdl_for_i(vector_table->Length)
@@ -268,6 +275,8 @@ static inline void vdl_VectorTableUntrackByIndex_BT(VDL_VECTOR_TABLE_T *const ve
     vdl_CheckIndexOutOfBound(vector_table, index);
 
     VDL_VECTOR_P v = vdl_UnsafeVectorPointerAt(vector_table, index);
+    vdl_CheckNullPointer(v);
+
     if (free_content)
     {
         vdl_Free(v->Data);
@@ -285,19 +294,8 @@ static inline void vdl_VectorTableUntrackByIndex_BT(VDL_VECTOR_TABLE_T *const ve
  |  Garbage collector
  ----------------------------------------------------------------------------*/
 
-static inline void vdl_CheckGarbageCollector_BT(void)
-{
-    if (((vdl_GlobalVar_VectorTable == NULL) +
-         (vdl_GlobalVar_DirectlyReachable == NULL) +
-         (vdl_GlobalVar_Reachable == NULL)) %
-        3)
-    {
-        vdl_Throw(VDL_EXCEPTION_INCONSISTENT_GARBAGE_COLLECTOR_STATE,
-                  "The garbage collector state is inconsistent!");
-    }
-}
 
-static inline void vdl_InitGarbageCollector_BT(void)
+static inline void vdl_GarbageCollectorInit_BT(void)
 {
     vdl_CheckGarbageCollector();
 
@@ -313,7 +311,7 @@ static inline void vdl_GarbageCollectorRecord_BT(VDL_VECTOR_T *const v)
 {
     vdl_CheckGarbageCollector();
     if (vdl_GlobalVar_VectorTable == NULL)
-        vdl_InitGarbageCollector();
+        vdl_GarbageCollectorInit();
 
     vdl_VectorTableRecord(vdl_GlobalVar_VectorTable, v);
 }
@@ -335,17 +333,24 @@ static inline void vdl_DeclareDirectlyUnreachable_BT(VDL_VECTOR_T *const v)
 static inline void vdl_UpdateReachable_BT(void)
 {
     vdl_CheckGarbageCollector();
-    vdl_CleanVectorTable_BT(vdl_GlobalVar_Reachable, 0);
+
+    vdl_ClearVectorTable_BT(vdl_GlobalVar_Reachable, 0);
 
     if (vdl_GlobalVar_DirectlyReachable->Length == 0)
         return;
 
-    // Record all the directly reachable objects by the reachable table.
+    // Ensure no null pointers in vector table.
     vdl_for_i(vdl_GlobalVar_DirectlyReachable->Length)
     {
-        vdl_VectorTableRecord(vdl_GlobalVar_Reachable,
-                              vdl_GlobalVar_DirectlyReachable->Data[i]);
+        vdl_CheckNullPointer(vdl_GlobalVar_DirectlyReachable->Data[i]);
     }
+
+    // Copy all the directly reachable objects to reachable table.
+    vdl_ReserveForVectorTable(vdl_GlobalVar_Reachable, vdl_GlobalVar_DirectlyReachable->Length);
+    memcpy(vdl_GlobalVar_Reachable->Data,
+           vdl_GlobalVar_DirectlyReachable->Data,
+           (size_t) vdl_GlobalVar_DirectlyReachable->Length * sizeof(VDL_VECTOR_P));
+    vdl_GlobalVar_Reachable->Length = vdl_GlobalVar_DirectlyReachable->Length;
 
     // Use BFS to find all reachable objects.
     int head_index = 0;
@@ -353,13 +358,17 @@ static inline void vdl_UpdateReachable_BT(void)
     while (head_index <= tail_index)
     {
         VDL_VECTOR_P head = vdl_GlobalVar_Reachable->Data[head_index];
-        vdl_CheckNullPointer(head);
 
         if (head->Type == VDL_TYPE_VECTOR_P)
         {
             vdl_CheckNullPointer(head->Data);
             VDL_VECTOR_POINTER_ARRAY vectors = head->Data;
-            vdl_for_i(head->Length) vdl_VectorTableRecord(vdl_GlobalVar_Reachable, vectors[i]);
+            vdl_for_i(head->Length)
+            {
+                // NA values should not be recorded by the reachable table.
+                if (vectors[i] != VDL_VECTOR_POINTER_NA)
+                    vdl_VectorTableRecord(vdl_GlobalVar_Reachable, vectors[i]);
+            }
             tail_index = vdl_GlobalVar_Reachable->Length - 1;
         }
 
@@ -372,6 +381,7 @@ static inline void vdl_GarbageCollectorCleanUp_BT(void)
     vdl_CheckGarbageCollector();
 
     vdl_UpdateReachable();
+
     int head_index                   = 0;
     VDL_VECTOR_POINTER_ARRAY vectors = vdl_GlobalVar_VectorTable->Data;
     while (head_index <= vdl_GlobalVar_VectorTable->Length - 1)
@@ -386,9 +396,15 @@ static inline void vdl_GarbageCollectorCleanUp_BT(void)
 static inline void vdl_GarbageCollectorKill_BT(void)
 {
     vdl_CheckGarbageCollector();
+
     vdl_DeleteVectorTable(vdl_GlobalVar_VectorTable, 1);
+    vdl_GlobalVar_VectorTable = NULL;
+
     vdl_DeleteVectorTable(vdl_GlobalVar_Reachable, 0);
+    vdl_GlobalVar_Reachable = NULL;
+
     vdl_DeleteVectorTable(vdl_GlobalVar_DirectlyReachable, 0);
+    vdl_GlobalVar_DirectlyReachable = NULL;
 }
 
 #endif//VDL_VDL_5_GARBAGE_COLLECTOR_DEF_H
