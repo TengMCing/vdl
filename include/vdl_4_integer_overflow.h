f596ea502aa9eb13a65f8dd252d5dacede64b041
TengMCing <llreczx@gmail.com>
2023-03-02 23:51:59 +1100

    add long to integer overflow list

--- include/vdl_4_integer_overflow.h
+++ include/vdl_4_integer_overflow.h
@@ -5,6 +5,24 @@
 #ifndef VDL_VDL_4_INTEGER_OVERFLOW_H
 #define VDL_VDL_4_INTEGER_OVERFLOW_H
 
+
+/*-----------------------------------------------------------------------------
+ |  Compile time check for Clang extension
+ ----------------------------------------------------------------------------*/
+
+#ifndef __has_builtin         // Optional of course.
+    #define __has_builtin(x) 0// Compatibility with non-clang compilers.
+#endif
+
+vdl_CompileTimeAssert(__has_builtin(__builtin_add_overflow), integer_overflow);
+vdl_CompileTimeAssert(__has_builtin(__builtin_sub_overflow), integer_overflow);
+vdl_CompileTimeAssert(__has_builtin(__builtin_mul_overflow), integer_overflow);
+
+
+/*-----------------------------------------------------------------------------
+ |  Integer overflow handle (int)
+ ----------------------------------------------------------------------------*/
+
 /// @description Safely add two integers. Integer overflow will cause an exception.
 /// @param x (int). An int.
 /// @param y (int). Another int.
@@ -25,5 +43,30 @@ static inline int vdl_SubIntOverflow_BT(int x, int y);
 /// @param (int) An int.
 #define vdl_MulIntOverflow(...) vdl_CallFunction(vdl_MulIntOverflow_BT, int, __VA_ARGS__)
 static inline int vdl_MulIntOverflow_BT(int x, int y);
+
+/*-----------------------------------------------------------------------------
+ |  Integer overflow handle (long)
+ ----------------------------------------------------------------------------*/
+
+/// @description Safely add two long integers. Integer overflow will cause an exception.
+/// @param x (long). A long.
+/// @param y (long). Another long.
+/// @param (long) A long.
+#define vdl_AddLongOverflow(...) vdl_CallFunction(vdl_AddLongOverflow_BT, int, __VA_ARGS__)
+static inline long vdl_AddLongOverflow_BT(long x, long y);
+
+/// @description Safely subtract two long integers. Integer overflow will cause an exception.
+/// @param x (long). A long.
+/// @param y (long). Another long.
+/// @param (long) A long.
+#define vdl_SubLongOverflow(...) vdl_CallFunction(vdl_SubLongOverflow_BT, int, __VA_ARGS__)
+static inline long vdl_SubLongOverflow_BT(long x, long y);
+
+/// @description Safely multiply two long integers. Integer overflow will cause an exception.
+/// @param x (long). A long.
+/// @param y (long). Another long.
+/// @param (long) A long.
+#define vdl_MulLongOverflow(...) vdl_CallFunction(vdl_MulLongOverflow_BT, int, __VA_ARGS__)
+static inline long vdl_MulLongOverflow_BT(long x, long y);
 
 #endif//VDL_VDL_4_INTEGER_OVERFLOW_H
--- include/vdl_4_integer_overflow_def.h
+++ include/vdl_4_integer_overflow_def.h
@@ -5,6 +5,10 @@
 #ifndef VDL_VDL_4_INTEGER_OVERFLOW_DEF_H
 #define VDL_VDL_4_INTEGER_OVERFLOW_DEF_H
 
+/*-----------------------------------------------------------------------------
+ |  Integer overflow handle (int)
+ ----------------------------------------------------------------------------*/
+
 
 static inline int vdl_AddIntOverflow_BT(const int x, const int y)
 {
@@ -31,6 +35,42 @@ static inline int vdl_SubIntOverflow_BT(const int x, const int y)
 static inline int vdl_MulIntOverflow_BT(const int x, const int y)
 {
     int z             = 0;
+    int overflow_flag = __builtin_mul_overflow(x, y, &z);
+    vdl_Expect(overflow_flag == 0,
+               VDL_EXCEPTION_INTEGER_OVERFLOW,
+               "Integer overflow occurred!");
+    return z;
+}
+
+/*-----------------------------------------------------------------------------
+ |  Integer overflow handle (long)
+ ----------------------------------------------------------------------------*/
+
+static inline long vdl_AddLongOverflow_BT(const long x, const long y)
+{
+    long z            = 0;
+    int overflow_flag = __builtin_add_overflow(x, y, &z);
+    vdl_Expect(overflow_flag == 0,
+               VDL_EXCEPTION_INTEGER_OVERFLOW,
+               "Integer overflow occurred!");
+    return z;
+}
+
+
+static inline long vdl_SubLongOverflow_BT(const long x, const long y)
+{
+    long z            = 0;
+    int overflow_flag = __builtin_sub_overflow(x, y, &z);
+    vdl_Expect(overflow_flag == 0,
+               VDL_EXCEPTION_INTEGER_OVERFLOW,
+               "Integer overflow occurred!");
+    return z;
+}
+
+
+static inline long vdl_MulLongOverflow_BT(const long x, const long y)
+{
+    long z            = 0;
     int overflow_flag = __builtin_mul_overflow(x, y, &z);
     vdl_Expect(overflow_flag == 0,
                VDL_EXCEPTION_INTEGER_OVERFLOW,
