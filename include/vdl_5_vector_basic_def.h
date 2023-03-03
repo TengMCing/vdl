a71cb4b1409a411da40271d2c137dfd4ef6b025f
TengMCing <llreczx@gmail.com>
2023-03-03 14:22:31 +1100

    use primitive
    Use primitive to indicate functions involve the use of primitive C types in input or output

--- include/vdl_5_vector_basic_def.h
+++ include/vdl_5_vector_basic_def.h
@@ -9,111 +9,111 @@
  |  Accessing the vector data safely
  ----------------------------------------------------------------------------*/
 
-static inline void *vdl_AddressOf_BT(VDL_VECTOR_T *const v, const int i)
+static inline void *vdl_vector_primitive_GetAddress_BT(VDL_VECTOR_P v, int i)
 {
     vdl_CheckNullVectorAndNullContainer(v);
     vdl_CheckIndexOutOfBound(v, i);
 
-    return vdl_UnsafeAddressOf(v, i);
+    return vdl_vector_primitive_UnsafeAddressOf(v, i);
 }
 
-static inline char vdl_GetChar_BT(VDL_VECTOR_T *const v, const int i)
+static inline char vdl_vector_primitive_GetChar_BT(VDL_VECTOR_P v, int i)
 {
     vdl_CheckNullVectorAndNullContainer(v);
     vdl_CheckIndexOutOfBound(v, i);
     vdl_CheckType(v->Type, VDL_TYPE_CHAR);
 
-    return vdl_UnsafeConstCharAt(v, i);
+    return vdl_vector_primitive_UnsafeConstCharAt(v, i);
 }
 
-static inline int vdl_GetInt_BT(VDL_VECTOR_T *const v, const int i)
+static inline int vdl_vector_primitive_GetInt_BT(VDL_VECTOR_P v, int i)
 {
     vdl_CheckNullVectorAndNullContainer(v);
     vdl_CheckIndexOutOfBound(v, i);
     vdl_CheckType(v->Type, VDL_TYPE_INT);
 
-    return vdl_UnsafeConstIntAt(v, i);
+    return vdl_vector_primitive_UnsafeConstIntAt(v, i);
 }
 
-static inline double vdl_GetDouble_BT(VDL_VECTOR_T *const v, const int i)
+static inline double vdl_vector_primitive_GetDouble_BT(VDL_VECTOR_P v, int i)
 {
     vdl_CheckNullVectorAndNullContainer(v);
     vdl_CheckIndexOutOfBound(v, i);
     vdl_CheckType(v->Type, VDL_TYPE_DOUBLE);
 
-    return vdl_UnsafeConstDoubleAt(v, i);
+    return vdl_vector_primitive_UnsafeConstDoubleAt(v, i);
 }
 
-static inline VDL_VECTOR_P vdl_GetVectorPointer_BT(VDL_VECTOR_T *const v, const int i)
+static inline VDL_VECTOR_P vdl_vector_primitive_GetVectorPointer_BT(VDL_VECTOR_P v, int i)
 {
     vdl_CheckNullVectorAndNullContainer(v);
     vdl_CheckIndexOutOfBound(v, i);
     vdl_CheckType(v->Type, VDL_TYPE_VECTOR_POINTER);
 
-    return vdl_UnsafeVectorConstPointerAt(v, i);
+    return vdl_vector_primitive_UnsafeVectorConstPointerAt(v, i);
 }
 
 /*-----------------------------------------------------------------------------
  |  Set the vector data safely
  ----------------------------------------------------------------------------*/
 
-static inline void vdl_SetChar_BT(VDL_VECTOR_T *const v, const int i, const char item)
+static inline void vdl_vector_primitive_SetChar_BT(VDL_VECTOR_P v, int i, char item)
 {
     vdl_CheckNullVectorAndNullContainer(v);
     vdl_CheckIndexOutOfBound(v, i);
     vdl_CheckType(v->Type, VDL_TYPE_CHAR);
 
-    vdl_UnsafeSetChar(v, i, item);
+    vdl_vector_primitive_UnsafeSetChar(v, i, item);
 }
 
-static inline void vdl_SetInt_BT(VDL_VECTOR_T *const v, const int i, const int item)
+static inline void vdl_vector_primitive_SetInt_BT(VDL_VECTOR_P v, int i, int item)
 {
     vdl_CheckNullVectorAndNullContainer(v);
     vdl_CheckIndexOutOfBound(v, i);
     vdl_CheckType(v->Type, VDL_TYPE_INT);
 
-    vdl_UnsafeSetInt(v, i, item);
+    vdl_vector_primitive_UnsafeSetInt(v, i, item);
 }
 
-static inline void vdl_SetDouble_BT(VDL_VECTOR_T *const v, const int i, const double item)
+static inline void vdl_vector_primitive_SetDouble_BT(VDL_VECTOR_P v, int i, double item)
 {
     vdl_CheckNullVectorAndNullContainer(v);
     vdl_CheckIndexOutOfBound(v, i);
     vdl_CheckType(v->Type, VDL_TYPE_DOUBLE);
 
-    vdl_UnsafeSetDouble(v, i, item);
+    vdl_vector_primitive_UnsafeSetDouble(v, i, item);
 }
 
-static inline void vdl_SetVectorPointer_BT(VDL_VECTOR_T *const v, const int i, VDL_VECTOR_T *const item)
+static inline void vdl_vector_primitive_SetVectorPointer_BT(VDL_VECTOR_P v, int i, VDL_VECTOR_P item)
 {
     vdl_CheckNullVectorAndNullContainer(v);
     vdl_CheckIndexOutOfBound(v, i);
     vdl_CheckType(v->Type, VDL_TYPE_VECTOR_POINTER);
 
-    vdl_UnsafeSetVectorPointer(v, i, item);
+    vdl_vector_primitive_UnsafeSetVectorPointer(v, i, item);
 }
 
-static inline void vdl_SetByArrayAndMemcpy_BT(VDL_VECTOR_P v, int i, const void *item_pointer, int number)
+static inline void vdl_vector_primitive_SetByArrayAndMemcpy_BT(VDL_VECTOR_P v, int i, const void *item_pointer, int number)
 {
     vdl_CheckNullVectorAndNullContainer(v);
     vdl_CheckNullArrayAndNegativeLength(item_pointer, number);
     vdl_CheckIndexOutOfBound(v, i);
     vdl_CheckIndexOutOfBound(v, i + number - 1);
 
-    vdl_UnsafeSetByArrayAndMemcpy(v, i, item_pointer, number);
+    vdl_vector_primitive_UnsafeSetByArrayAndMemcpy(v, i, item_pointer, number);
 }
 
-static inline void vdl_SetByArrayAndMemmove_BT(VDL_VECTOR_P v, int i, const void *item_pointer, int number)
+static inline void vdl_vector_primitive_SetByArrayAndMemmove_BT(VDL_VECTOR_P v, int i, const void *item_pointer, int number)
 {
     vdl_CheckNullVectorAndNullContainer(v);
     vdl_CheckNullArrayAndNegativeLength(item_pointer, number);
     vdl_CheckIndexOutOfBound(v, i);
     vdl_CheckIndexOutOfBound(v, i + number - 1);
 
-    vdl_UnsafeSetByArrayAndMemmove(v, i, item_pointer, number);
+    vdl_vector_primitive_UnsafeSetByArrayAndMemmove(v, i, item_pointer, number);
 }
 
-static inline void vdl_SetCharByArrayAndIndex_BT(VDL_VECTOR_P v, const char *item_pointer, const int *index_pointer, int number)
+static inline void vdl_vector_primitive_SetCharByArrayAndIndex_BT(VDL_VECTOR_P v, const char *item_pointer, const int *index_pointer, int number)
 {
     vdl_CheckNullVectorAndNullContainer(v);
     vdl_CheckNullArrayAndNegativeLength(item_pointer, number);
@@ -122,10 +122,10 @@ static inline void vdl_SetCharByArrayAndIndex_BT(VDL_VECTOR_P v, const char *ite
 
     vdl_for_i(number) vdl_CheckIndexOutOfBound(v, index_pointer[i]);
 
-    vdl_UnsafeSetCharByArrayAndIndex(v, item_pointer, index_pointer, number);
+    vdl_vector_primitive_UnsafeSetCharByArrayAndIndex(v, item_pointer, index_pointer, number);
 }
 
-static inline void vdl_SetIntByArrayAndIndex_BT(VDL_VECTOR_P v, const int *item_pointer, const int *index_pointer, int number)
+static inline void vdl_vector_primitive_SetIntByArrayAndIndex_BT(VDL_VECTOR_P v, const int *item_pointer, const int *index_pointer, int number)
 {
     vdl_CheckNullVectorAndNullContainer(v);
     vdl_CheckNullArrayAndNegativeLength(item_pointer, number);
@@ -134,10 +134,10 @@ static inline void vdl_SetIntByArrayAndIndex_BT(VDL_VECTOR_P v, const int *item_
 
     vdl_for_i(number) vdl_CheckIndexOutOfBound(v, index_pointer[i]);
 
-    vdl_UnsafeSetIntByArrayAndIndex(v, item_pointer, index_pointer, number);
+    vdl_vector_primitive_UnsafeSetIntByArrayAndIndex(v, item_pointer, index_pointer, number);
 }
 
-static inline void vdl_SetDoubleByArrayAndIndex_BT(VDL_VECTOR_P v, const double *item_pointer, const int *index_pointer, int number)
+static inline void vdl_vector_primitive_SetDoubleByArrayAndIndex_BT(VDL_VECTOR_P v, const double *item_pointer, const int *index_pointer, int number)
 {
     vdl_CheckNullVectorAndNullContainer(v);
     vdl_CheckNullArrayAndNegativeLength(item_pointer, number);
@@ -146,10 +146,10 @@ static inline void vdl_SetDoubleByArrayAndIndex_BT(VDL_VECTOR_P v, const double 
 
     vdl_for_i(number) vdl_CheckIndexOutOfBound(v, index_pointer[i]);
 
-    vdl_UnsafeSetDoubleByArrayAndIndex(v, item_pointer, index_pointer, number);
+    vdl_vector_primitive_UnsafeSetDoubleByArrayAndIndex(v, item_pointer, index_pointer, number);
 }
 
-static inline void vdl_SetVectorPointerByArrayAndIndex_BT(VDL_VECTOR_P v, VDL_VECTOR_T *const *item_pointer, const int *index_pointer, int number)
+static inline void vdl_vector_primitive_SetVectorPointerByArrayAndIndex_BT(VDL_VECTOR_P v, VDL_VECTOR_T *const *item_pointer, const int *index_pointer, int number)
 {
     vdl_CheckNullVectorAndNullContainer(v);
     vdl_CheckNullArrayAndNegativeLength(item_pointer, number);
@@ -158,14 +158,14 @@ static inline void vdl_SetVectorPointerByArrayAndIndex_BT(VDL_VECTOR_P v, VDL_VE
 
     vdl_for_i(number) vdl_CheckIndexOutOfBound(v, index_pointer[i]);
 
-    vdl_UnsafeSetVectorPointerByArrayAndIndex(v, item_pointer, index_pointer, number);
+    vdl_vector_primitive_UnsafeSetVectorPointerByArrayAndIndex(v, item_pointer, index_pointer, number);
 }
 
 /*-----------------------------------------------------------------------------
  |  Set the vector data safely (Public API)
  ----------------------------------------------------------------------------*/
 
-static inline void vdl_Set_BT(VDL_VECTOR_T *const v1, VDL_VECTOR_T *const v2)
+static inline void vdl_vector_Set_BT(VDL_VECTOR_P v1, VDL_VECTOR_P v2)
 {
     vdl_CheckNullVectorAndNullContainer(v1);
     vdl_CheckNullVectorAndNullContainer(v2);
@@ -188,7 +188,7 @@ static inline void vdl_Set_BT(VDL_VECTOR_T *const v1, VDL_VECTOR_T *const v2)
             }
             else
             {
-                vdl_SetByArrayAndMemmove(v1, 0, v2->Data, v1->Length);
+                vdl_vector_primitive_SetByArrayAndMemmove(v1, 0, v2->Data, v1->Length);
             }
             break;
         }
@@ -202,7 +202,7 @@ static inline void vdl_Set_BT(VDL_VECTOR_T *const v1, VDL_VECTOR_T *const v2)
             }
             else
             {
-                vdl_SetByArrayAndMemmove(v1, 0, v2->Data, v1->Length);
+                vdl_vector_primitive_SetByArrayAndMemmove(v1, 0, v2->Data, v1->Length);
             }
             break;
         }
@@ -216,7 +216,7 @@ static inline void vdl_Set_BT(VDL_VECTOR_T *const v1, VDL_VECTOR_T *const v2)
             }
             else
             {
-                vdl_SetByArrayAndMemmove(v1, 0, v2->Data, v1->Length);
+                vdl_vector_primitive_SetByArrayAndMemmove(v1, 0, v2->Data, v1->Length);
             }
             break;
         }
@@ -230,14 +230,14 @@ static inline void vdl_Set_BT(VDL_VECTOR_T *const v1, VDL_VECTOR_T *const v2)
             }
             else
             {
-                vdl_SetByArrayAndMemmove(v1, 0, v2->Data, v1->Length);
+                vdl_vector_primitive_SetByArrayAndMemmove(v1, 0, v2->Data, v1->Length);
             }
             break;
         }
     }
 }
 
-static inline void vdl_SetByIndex_BT(VDL_VECTOR_T *const v1, VDL_VECTOR_T *const i, VDL_VECTOR_T *const v2)
+static inline void vdl_vector_SetByIndex_BT(VDL_VECTOR_P v1, VDL_VECTOR_P i, VDL_VECTOR_P v2)
 {
     vdl_CheckNullVectorAndNullContainer(v1);
     vdl_CheckNullVectorAndNullContainer(i);
@@ -322,13 +322,13 @@ static inline void vdl_SetByIndex_BT(VDL_VECTOR_T *const v1, VDL_VECTOR_T *const
  |  Convert an integer vector to a boolean scalar value
  ----------------------------------------------------------------------------*/
 
-static inline int vdl_ToBoolScalar(VDL_VECTOR_T *const v)
+static inline int vdl_vector_primitive_ToBool_BT(VDL_VECTOR_P condition)
 {
-    vdl_CheckNullVectorAndNullContainer(v);
-    vdl_CheckType(v->Type, VDL_TYPE_INT);
-    vdl_CheckLength(v->Length, 1);
+    vdl_CheckNullVectorAndNullContainer(condition);
+    vdl_CheckType(condition->Type, VDL_TYPE_INT);
+    vdl_CheckLength(condition->Length, 1);
 
-    return vdl_GetInt(v, 0) != 0;
+    return vdl_vector_primitive_UnsafeIntAt(condition, 0) != 0;
 }
 
 
