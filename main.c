f280ed7595b2bf2f61f249f4cf82104b215d6249
TengMCing <llreczx@gmail.com>
2023-03-07 15:51:34 +1100

    update main.c

--- main.c
+++ main.c
@@ -7,14 +7,14 @@
 
 int main(void)
 {
-    vdl_NewByDoubleScalar(1.1);
-    VDL_VECTOR_P v = vdl_New(vdl_New(1, 2, 3),
-                             vdl_New(1.1, 2.1, 3.1),
-                             vdl_New(NULL));
-    vdl_UnsafeSetVectorPointer(vdl_GetVectorPointer(v, 2), 0, v);
-    printf("%f\n", vdl_GetDouble(vdl_GetVectorPointer(v, 1), 2));
+    vdl_vector_primitive_NewByDouble(1.1);
+    VDL_VECTOR_P v = vdl_vector_primitive_New(vdl_vector_primitive_New(1, 2, 3),
+                                              vdl_vector_primitive_New(1.1, 2.1, 3.1),
+                                              vdl_vector_primitive_New(NULL));
+    vdl_vector_primitive_UnsafeSetVectorPointer(vdl_vector_primitive_GetVectorPointer(v, 2), 0, v);
+    printf("%f\n", vdl_vector_primitive_GetDouble(vdl_vector_primitive_GetVectorPointer(v, 1), 2));
     vdl_PrintVectorTable(vdl_GlobalVar_VectorTable);
-    vdl_DeclareDirectlyReachable(vdl_GetVectorPointer(v, 2));
+    vdl_DeclareDirectlyReachable(vdl_vector_primitive_GetVectorPointer(v, 2));
     vdl_GarbageCollectorCleanUp();
     vdl_PrintVectorTable(vdl_GlobalVar_VectorTable);
     return 0;
